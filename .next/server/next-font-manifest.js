007e804363a25aa359ee2fb03c431443b14ce67b
Philip Bilén <philipperbilen@gmail.com>
2025-06-04 21:17:26 +0200

    v2

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/philip/Desktop/engeloop-website/engeloop-records/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/b0088cce7ac0b424-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
